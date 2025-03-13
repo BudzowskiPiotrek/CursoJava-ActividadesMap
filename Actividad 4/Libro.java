579cab4a9bcbcdfcc044dbea688cd9534b536c85
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-14 00:01:59 +0100

    Create Libro.java

---
+++ Actividad 4/Libro.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Libro {
+	private String titulo;
+	private String autor;
+
+	public Libro(String titulo, String autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autor, titulo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		return Objects.equals(autor, other.autor) && Objects.equals(titulo, other.titulo);
+	}
+
+	@Override
+	public String toString() {
+		return "Titulo= " + titulo + ", autor= " + autor;
+	}
+
+}
